48ca23f5064fd17b310e809002ee51504e47ce2e
Edoardo Gargano <cnt000@gmal.com>
2024-03-21 15:46:24 +0100

    shadow dom and root

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # BOX CHART
 
 Simple webcomponents example for drawing a microbiome graph.
+
+```html
+<p>
+    <h2>Example 1:</h2>
+    <box-chart
+    height="40"
+    box-start="200"
+    box-width="300"
+    dot-position="280"
+    ></box-chart>
+</p>
+
+<p>
+    <h2>Example 2:</h2>
+    <box-chart
+    height="40"
+    box-start="500"
+    box-width="100"
+    dot-position="280"
+    ></box-chart>
+</p>
+```
--- box-chart.js
+++ box-chart.js
@@ -1,6 +1,16 @@
 class BoxChart extends HTMLElement {
   constructor() {
     super();
+    this.containerMinWidth = Number(this.getAttribute("minwidth")) || 800;
+    this.containerHeight = Number(this.getAttribute("height")) || 40;
+    this.boxStart = Number(this.getAttribute("box-start")) || 120;
+    this.boxWidth = Number(this.getAttribute("box-width")) || 200;
+    this.dotPosition = (Number(this.getAttribute("dot-position")) || 0) - 8;
+
+    this.isDotRed =
+      Number(this.dotPosition) >
+        Number(this.boxStart) + Number(this.boxWidth) ||
+      Number(this.dotPosition) < Number(this.boxStart);
   }
 
   connectedCallback() {
@@ -8,70 +18,61 @@ class BoxChart extends HTMLElement {
   }
 
   render() {
-    const now = new Date();
-    const containerMinWidth = this.getAttribute("minwidth") || 800;
-    const containerHeight = this.getAttribute("height") || 40;
-    const boxStart = this.getAttribute("box-start") || 120;
-    const boxWidth = this.getAttribute("box-width") || 200;
-    const dotPosition = this.getAttribute("dot-position") || 0;
-
-    const isDotRed =
-      Number(dotPosition) > Number(boxStart) + Number(boxWidth) ||
-      Number(dotPosition) < Number(boxStart);
-
-    // this.attachShadow({ mode: "open" });
-    this.innerHTML = `
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
       <style>
-        .container {
-          width: 100%;
-          min-width: ${containerMinWidth}px;
-          height: ${containerHeight}px;
-          background-color: lightgoldenrodyellow;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          position: relative;
-        }
-        .line {
-          height: 2px;
-          width: 96%;
-          background-color: black;
-        }
-        .box {
-          position: absolute;
-          left: ${boxStart}px;
-          height: 20px;
-          width: ${boxWidth}px;
-          border: 2px solid grey;
-          background-color: lightgray;
-        }
-        .dot {
-          position: absolute;
-          left: ${dotPosition}px;
-          border-radius: 50%;
-          width: 16px;
-          height: 16px;
-          background-color: black;
-        }
-        .exclamation {
-          position: absolute;
-          right: 8px;
-          font-size: 2.5rem;
-          font-family: arial;
-          color: red;
-        }
-        .red {
-          background-color: red;
-        }
-        .hidden {
-          display: none;
-        }
+      .container {
+        margin: 0 auto;
+        width: 90%;
+        min-width: ${this.containerMinWidth}px;
+        height: ${this.containerHeight}px;
+        background-color: lightgoldenrodyellow;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+      }
+      .line {
+        height: 2px;
+        width: 100%;
+        margin-left: 0;
+        background-color: black;
+      }
+      .box {
+        position: absolute;
+        left: ${this.boxStart}px;
+        height: 20px;
+        width: ${this.boxWidth}px;
+        border: 2px solid grey;
+        background-color: lightgray;
+      }
+      .dot {
+        position: absolute;
+        left: ${this.dotPosition}px;
+        border-radius: 50%;
+        width: 16px;
+        height: 16px;
+        background-color: black;
+      }
+      .exclamation {
+        position: absolute;
+        right: -20px;
+        font-size: 2.5rem;
+        font-family: arial;
+        color: red;
+      }
+      .red {
+        background-color: red;
+      }
+      .hidden {
+        display: none;
+      }
       </style>
       <div class="container">
         <div class="line"></div>
         <div class="box"></div>
-        <div class="dot ${isDotRed ? "red" : ""}"></div>
-        <div class="${isDotRed ? "" : "hidden"} exclamation">!</div>
+        <div class="dot ${this.isDotRed ? "red" : ""}"></div>
+        <div class="${this.isDotRed ? "" : "hidden"} exclamation">!</div>
       </div>`;
   }
 }
