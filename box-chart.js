e0dfe367d119addf6966a638d76c60f384020fbf
Edoardo Gargano <cnt000@gmal.com>
2024-03-21 15:57:18 +0100

    styles

--- box-chart.js
+++ box-chart.js
@@ -3,14 +3,15 @@ class BoxChart extends HTMLElement {
     super();
     this.containerMinWidth = Number(this.getAttribute("minwidth")) || 800;
     this.containerHeight = Number(this.getAttribute("height")) || 40;
-    this.boxStart = Number(this.getAttribute("box-start")) || 120;
-    this.boxWidth = Number(this.getAttribute("box-width")) || 200;
+    this.boxStart = (Number(this.getAttribute("box-start")) || 120) - 1;
+    this.boxWidth = (Number(this.getAttribute("box-width")) || 200) - 2;
     this.dotPosition = (Number(this.getAttribute("dot-position")) || 0) - 8;
+    this.trianglePosition =
+      (Number(this.getAttribute("triangle-position")) || 0) - 8;
 
     this.isDotRed =
-      Number(this.dotPosition) >
-        Number(this.boxStart) + Number(this.boxWidth) ||
-      Number(this.dotPosition) < Number(this.boxStart);
+      this.dotPosition > this.boxStart + this.boxWidth ||
+      this.dotPosition < this.boxStart;
   }
 
   connectedCallback() {
@@ -21,12 +22,15 @@ class BoxChart extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>
+      * {
+        box-sizing: border-box;
+      }
       .container {
         margin: 0 auto;
         width: 90%;
         min-width: ${this.containerMinWidth}px;
         height: ${this.containerHeight}px;
-        background-color: lightgoldenrodyellow;
+        background-color: limegreen;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -41,7 +45,7 @@ class BoxChart extends HTMLElement {
       .box {
         position: absolute;
         left: ${this.boxStart}px;
-        height: 20px;
+        height: 30px;
         width: ${this.boxWidth}px;
         border: 2px solid grey;
         background-color: lightgray;
@@ -53,6 +57,15 @@ class BoxChart extends HTMLElement {
         width: 16px;
         height: 16px;
         background-color: black;
+      }
+      .triangle {
+        position: absolute;
+        left: ${this.trianglePosition}px;
+        width: 0;
+        height: 0;
+        border: 8px solid transparent;
+        border-top: 0;
+        border-bottom: 16px solid #ffe900;
       }
       .exclamation {
         position: absolute;
@@ -71,6 +84,7 @@ class BoxChart extends HTMLElement {
       <div class="container">
         <div class="line"></div>
         <div class="box"></div>
+        <div class="triangle"></div>
         <div class="dot ${this.isDotRed ? "red" : ""}"></div>
         <div class="${this.isDotRed ? "" : "hidden"} exclamation">!</div>
       </div>`;
