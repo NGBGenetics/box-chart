4cbde9e699ff73a345c7a9bc296a3d416a682e21
Edoardo Gargano <cnt000@gmal.com>
2024-04-26 14:58:23 +0200

    distributed pixel

--- multi-box-chart.js
+++ multi-box-chart.js
@@ -1,10 +1,7 @@
 class MultiBoxChart extends HTMLElement {
+  // TODO deve mettere le label x delle box, mediane e limits, mettere le label dei dots vicino ai pallini
   defaultWidth = 600;
-  defaultHeight = 300;
-  defaultDots = [0, 80, 90, 100, 300, 450];
-  defaultBox = [100, 300];
-  defaultLimit = [50, 400];
-  defaultMediane = 200;
+  defaultDots = [150];
   xScaleSteps = 10;
 
   constructor() {
@@ -13,24 +10,48 @@ class MultiBoxChart extends HTMLElement {
 
   connectedCallback() {
     this.width = Number(this.getAttribute("width")) || this.defaultWidth;
-    this.height = Number(this.getAttribute("height")) || this.defaultHeight;
     this.dots =
       this.getAttribute("dots")?.replaceAll(" ", "").split(",").map(Number) ||
       this.defaultDots;
-    this.mediane = Number(this.getAttribute("mediane")) || this.defaultMediane;
-    [this.boxStart, this.boxSize] =
-      this.getAttribute("box")?.replaceAll(" ", "").split(",").map(Number) ||
-      this.defaultBox;
-    [this.limitStart, this.limitEnd] =
-      this.getAttribute("limit")?.replaceAll(" ", "").split(",").map(Number) ||
-      this.defaultLimit;
+    const box = this.getAttribute("box")
+      ?.replaceAll(" ", "")
+      .split(",")
+      .map(Number);
+    const limit = this.getAttribute("limit")
+      ?.replaceAll(" ", "")
+      .split(",")
+      .map(Number);
+    this.times = this.getAttribute("time")?.replaceAll(" ", "").split(",");
+    this.mediane = Number(this.getAttribute("mediane"));
+    this.maxValue = Math.max(
+      ...this.dots,
+      ...(box ? box : []),
+      ...(limit ? limit : []),
+      this.mediane || []
+    );
+    this.distributedMediane = this.getPixelValue(this.mediane);
+    this.distributedDots = this.dots.map((dot) =>
+      this.getPixelValue(dot, this.maxValue, this.width)
+    );
+    this.height = Number(this.getAttribute("height")) || this.dots.length * 50;
+    this.boxStart = box?.[0];
+    this.boxEnd = box?.[1];
+    this.distributedBoxStart = this.getPixelValue(this.boxStart);
+    this.distributedBoxEnd = this.getPixelValue(this.boxEnd);
+    this.limitStart = limit?.[0];
+    this.limitEnd = limit?.[1];
+    this.distributedLimitStart = this.getPixelValue(this.limitStart);
+    this.distributedLimitEnd = this.getPixelValue(this.limitEnd);
     this.xScaleSteps = Number(this.getAttribute("x-steps")) || this.xScaleSteps;
     this.bottomStep = this.height / this.dots.length;
     this.render();
   }
 
+  getPixelValue(value) {
+    return (value / this.maxValue) * 0.9 * this.width;
+  }
+
   render() {
-    // TODO arancioni
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>
@@ -79,14 +100,16 @@ class MultiBoxChart extends HTMLElement {
           height: calc(${this.height}px + 28px);
           border-left: 2px solid var(--limit-color);
           position: absolute;
-          left: calc(${this.limitStart}px - 2px);
+          left: calc(${this.distributedLimitStart}px - 2px);
           bottom: -14px;
         }
 
         .limit::after {
           content: "";
           display: block;
-          left: calc(${this.limitEnd}px - 2px);
+          left: calc(${
+            this.distributedLimitEnd - this.distributedLimitStart
+          }px - 2px);
           bottom: 0;
         }
 
@@ -96,24 +119,45 @@ class MultiBoxChart extends HTMLElement {
           width: 2px;
           height: ${this.height}px;
           position: absolute;
-          left: calc(${this.mediane}px - 2px);
+          left: calc(${this.distributedMediane}px - 2px);
           bottom: 0px;
           background-color: var(--mediane-color);
           z-index: 1;
         }
 
         .box {
-          width: ${this.boxSize}px;
+          width: ${this.distributedBoxEnd - this.distributedBoxStart}px;
           height: ${this.height}px;
           background: var(--box-color);
           position: absolute;
           bottom: 0;
-          left: calc(${this.boxStart}px);
+          left: calc(${this.distributedBoxStart}px);
         }
 
         .x-label > span {
           position: absolute;
           bottom: -34px;
+          transform: translateX(-50%);
+        }
+
+        .x-label-mediane {
+          left: calc(${this.distributedMediane}px - 2px);
+        }
+
+        .x-label-limit-start {
+          left: calc(${this.distributedLimitStart}px - 2px);
+        }
+
+        .x-label-limit-end {
+          left: calc(${this.distributedLimitEnd}px - 2px);
+        }
+
+        .x-label-box-start {
+          left: calc(${this.distributedBoxStart}px - 2px);
+        }
+
+        .x-label-box-end {
+          left: calc(${this.distributedBoxEnd}px - 2px);
         }
 
         .y-label > span {
@@ -122,27 +166,21 @@ class MultiBoxChart extends HTMLElement {
 
         .y-label > span {
           display: block;
-          width: 10px;
           height: 10px;
           position: absolute;
-          left: -30px;
+          left: 0;
           bottom: -2px;
+          transform: translateX(calc(-100% - 10px));
         }
 
-        .y-label > span::after {
-          content: "";
+        .y-line > span {
           display: block;
           width: ${this.width}px;
-          height: 10px;
           border-bottom: 2px dashed gray;
           position: absolute;
-          left: 30px;
+          left: 0;
           bottom: 0;
           z-index: 1;
-        }
-
-        .y-label > span::after {
-          bottom: -2px;
         }
 
         .dots > div {
@@ -154,8 +192,15 @@ class MultiBoxChart extends HTMLElement {
           z-index: 2;
         }
 
+        .dot-label {
+          position: absolute;
+          left: 14px;
+          top: 14px;
+          font-size: 12px;
+        }
+
         /* DOTS POSITION AND BACKGROUND */   
-        ${this.dots
+        ${this.distributedDots
           .map((dot, i) => {
             return `.dots > div:nth-child(${i + 1}) {
                     bottom: calc(${this.bottomStep * (i + 1)}px - 1px);
@@ -167,39 +212,52 @@ class MultiBoxChart extends HTMLElement {
           .join("")}
 
         /* Y LABELS BOTTOM */
-        ${this.dots
+        ${this.distributedDots
           .map((_, i) => {
             return `.y-label > span:nth-child(${i + 1}) {
-                      bottom: ${this.bottomStep * (i + 1)}px;
+                      bottom: ${this.bottomStep * (i + 1) - 2}px;
                     }\n`;
           })
           .join("")}
 
-        /* X LABELS LEFT */
-        ${Array.from({ length: this.xScaleSteps })
+
+        /* Y LINE BOTTOM */
+        ${this.distributedDots
           .map((_, i) => {
-            return `.x-label > span:nth-child(${i + 1}) {
-            left: calc(${Math.floor((this.width / this.xScaleSteps) * i)}px);
-            transform: translateX(-50%);
-          }\n`;
+            return `.y-line > span:nth-child(${i + 1}) {
+                      bottom: ${this.bottomStep * (i + 1) - 2}px;
+                    }\n`;
           })
           .join("")}
-
       </style>
       <div class="axis" id="axis">
         <div class="y-label">
-          ${this.printElements("span", this.dots.length, (i) => `T${i + 1}`)}
+          ${this.printElements(
+            "span",
+            this.distributedDots.length,
+            (i) => this.times?.[i] || `T${i + 1}`
+          )}
+        </div>
+        <div class="y-line">
+          ${this.printElements("span", this.distributedDots.length, (i) => "")}
         </div>
 
         <div class="box"></div>
         <div class="limit"></div>
         <div class="mediane"></div>
-        <div class="dots">${this.dots.map(() => `<div></div>`).join("")}</div>
+        <div class="dots">${this.dots
+          .map(
+            (dot) =>
+              `<div><span class="dot-label">${dot.toFixed(2)}</span></div>`
+          )
+          .join("")}</div>
 
         <div class="x-label">
-        ${this.printElements("span", this.xScaleSteps, (i) =>
-          Math.floor((this.width / this.xScaleSteps) * i)
-        )}
+          <span class="x-label-mediane">${this.mediane}</span>
+          <span class="x-label-limit-start">${this.limitStart}</span>
+          <span class="x-label-limit-end">${this.limitEnd}</span>
+          <span class="x-label-box-start">${this.boxStart}</span>
+          <span class="x-label-box-end">${this.boxEnd}</span>
         </div>
       </div>
     `;
@@ -214,15 +272,15 @@ class MultiBoxChart extends HTMLElement {
   }
 
   printBackground(i, dotXPosition) {
-    const isLast = i === this.dots.length - 1;
+    const isLast = i === this.distributedDots.length - 1;
     const isInside =
       this.boxStart <= dotXPosition &&
-      this.boxStart + this.boxSize >= dotXPosition;
+      this.boxStart + this.boxEnd >= dotXPosition;
 
     const isClose =
       (this.boxStart - 30 <= dotXPosition && this.boxStart > dotXPosition) ||
-      (this.boxStart + this.boxSize + 30 >= dotXPosition &&
-        this.boxStart + this.boxSize < dotXPosition);
+      (this.boxStart + this.boxEnd + 30 >= dotXPosition &&
+        this.boxStart + this.boxEnd < dotXPosition);
 
     if (isClose) {
       return isLast
